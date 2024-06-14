fix: fix return rate calculation

DataPrep.py
@@ -1,11 +1,7 @@
 from typing import List
 
-from PairArbi import find_cointegration, ArbitragePair
-
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import urllib.request as urllib2
 import json
 
@@ -18,6 +14,7 @@ def get_data_from_csv(file_path: str, symbols: List[str]):
     for symbol in symbols:
         tmp_group = groupby_symbol.get_group(symbol)
         tmp_df = pd.DataFrame([np.array(tmp_group['Trddt']), np.log(np.array(tmp_group['Close']))]).T
+        # tmp_df = pd.DataFrame([np.array(tmp_group['Trddt']), np.array(tmp_group['Close'])]).T
         tmp_df.set_index(0, inplace=True)
         tmp_df.index.rename('date', inplace=True)
         tmp_df.columns = [symbol]
@@ -27,13 +24,8 @@ def get_data_from_csv(file_path: str, symbols: List[str]):
         merged_data = pd.merge(tmp_df, merged_data, left_index=True, right_index=True, how='inner')
     merged_data.index = pd.to_datetime(merged_data.index, format='%Y/%m/%d')
     merged_data = merged_data.astype(float)
-
+    merged_data.to_csv('log_price.csv')
     return merged_data
-
-
-
-
-# 'SC2403','SC2406','SC2409','SC2412'])
 
 def get_data(symbols: List[str], ):
     url_5m = 'http://stock2.finance.sina.com.cn/futures/api/json.php/IndexService.getInnerFuturesMiniKLine5m?symbol='

Strategy.py
@@ -15,9 +15,11 @@ class TradeSignal(Enum):
 
 
 class Strategy:
-    def __init__(self, pair: ArbitragePair, trading_period: tuple, open_threshold: float = 1.1,
-                 close_threshold: float = 0.3, stop_loss_threshold: float = 2.1, ):
+    def __init__(self, pair: ArbitragePair, trading_period: tuple, principle: float, open_threshold: float = 1.1,
+                 close_threshold: float = 0.5, stop_loss_threshold: float = 2.1, ):
         self.pair = pair
+        self.SC2306 = pair.coin1.apply(np.exp)
+        self.SC2303 = pair.coin2.apply(np.exp)*pair.OLS_results.params[1]
         self.trading_period = trading_period
         self.open_threshold = open_threshold
         self.close_threshold = close_threshold
@@ -27,15 +29,21 @@ class Strategy:
         self.spread = None
         self.trade_signals = None
         self.profit = None
+        self.principle = principle
 
     def trade(self):
         t_start, t_end = self.trading_period
         spread = self.pair.coin1.loc[t_start:t_end] - self.OLS_results.params[1] * self.pair.coin2.loc[t_start:t_end]
-        self.spread = spread
         standardized_spread = (spread - spread.mean()) / spread.std()
-        standardized_spread.name = 'spread'
-        # standardized_spread.to_csv('spread.csv')
+
+        delog_spread = self.pair.coin1.loc[t_start:t_end].apply(np.exp) - self.OLS_results.params[
+            1] * self.pair.coin2.loc[t_start:t_end].apply(np.exp)
+        delog_spread.name = 'spread'
+        delog_spread.to_csv('spread_delog.csv')
+        self.spread = delog_spread
         self.std_spread = standardized_spread
+
+
         # trading
         # open position
         open_position = False
@@ -67,65 +75,76 @@ class Strategy:
                 trade_signals.loc[i] = TradeSignal.STOP_LOSS
                 open_position = False
         self.trade_signals = trade_signals
-        # pd.concat([spread, trade_signals], axis=1, join='inner').to_csv('trade_signals.csv')
+        pd.concat([spread, trade_signals], axis=1, join='inner').to_csv('trade_signals_delog.csv')
         self.get_profit()
         print(f'Profit: {self.profit}')
 
     def get_profit(self):
+        principle = self.principle
         profit = 0
         open_idx = None
         profit_list = []
         open = False
-        for i, trade_signal in zip(range(0,len(self.trade_signals)), self.trade_signals):
+        hand = 0
+        for i, trade_signal in zip(range(0, len(self.trade_signals)), self.trade_signals):
             if trade_signal == TradeSignal.LONG:
                 open_idx = i
+                hand = principle / self.SC2306.iloc[i]
                 open = True
             if trade_signal == TradeSignal.SHORT:
                 open_idx = i
+                hand = principle / self.SC2303.iloc[i]
                 open = True
             if trade_signal == TradeSignal.STOP_LOSS and open:
-                profit -= np.abs(self.spread.iloc[open_idx] - self.spread.iloc[i])
+                profit -= hand*np.abs(self.spread.iloc[open_idx] - self.spread.iloc[i])
                 open = False
+                hand = 0
             if trade_signal == TradeSignal.CLOSE:
-                profit += np.abs(self.spread.iloc[open_idx] - self.spread.iloc[i])
+                profit += hand * np.abs(self.spread.iloc[open_idx] - self.spread.iloc[i])
                 open = False
+                hand = 0
         # get the daily return
-        LONG , SHORT= False, False
-        for i, trade_signal in zip(range(0,len(self.trade_signals)), self.trade_signals):
+        LONG, SHORT = False, False
+        for i, trade_signal in zip(range(0, len(self.trade_signals)), self.trade_signals):
             if trade_signal == TradeSignal.LONG:
                 LONG = True
                 profit_list.append(0)
+                hand = principle / self.SC2306.iloc[i]
             elif trade_signal == TradeSignal.SHORT:
                 SHORT = True
                 profit_list.append(0)
+                hand = principle / self.SC2303.iloc[i]
             elif trade_signal == TradeSignal.HOLD and LONG:
-                profit_list.append(self.spread.iloc[i] - self.spread.iloc[i-1])
+                profit_list.append(hand*(self.spread.iloc[i] - self.spread.iloc[i - 1])/principle)
             elif trade_signal == TradeSignal.HOLD and SHORT:
-                profit_list.append(self.spread.iloc[i-1] - self.spread.iloc[i])
+                profit_list.append(hand*(self.spread.iloc[i - 1] - self.spread.iloc[i])/principle)
             elif trade_signal == TradeSignal.CLOSE:
-                profit_list.append(abs(self.spread.iloc[i] - self.spread.iloc[i-1]))
+                profit_list.append(hand*abs(self.spread.iloc[i] - self.spread.iloc[i - 1])/principle)
                 LONG, SHORT = False, False
+                hand = 0
             elif trade_signal == TradeSignal.STOP_LOSS and (LONG or SHORT):
                 print('Stop loss')
-                profit_list.append(-abs(self.spread.iloc[i] - self.spread.iloc[i-1]))
+                profit_list.append(-hand*abs(self.spread.iloc[i] - self.spread.iloc[i - 1])/principle)
                 LONG, SHORT = False, False
+                hand = 0
             elif trade_signal == TradeSignal.HOLD and not LONG and not SHORT:
                 profit_list.append(0)
             else:
                 profit_list.append(0)
         profit_list = np.array(profit_list)
-        ER = np.exp(np.mean(profit_list)*252)-1-0.03
-        print(ER)
-        std = np.std(profit_list)*np.sqrt(252)
-        print((ER)/std)
-        downside_std = np.sqrt(np.mean(np.minimum(profit_list, 0) ** 2))*np.sqrt(252)
-        print(ER/downside_std)
-        cumulative_returns = np.exp(np.cumsum(profit_list))
+        print("profit_list", np.mean(profit_list))
+        ER = np.mean(profit_list) * 252-.03
+        print("Expected Return:", ER)
+        std = np.std(profit_list) * np.sqrt(252)
+        print("Sharp Ratio:", ER / std)
+        downside_std = np.sqrt(np.mean(np.minimum(profit_list, 0) ** 2)) * np.sqrt(252)
+        print("Sortino Ratio:", ER / downside_std)
+        cumulative_returns = np.cumsum(profit_list)
         max_drawdown = (cumulative_returns.max() - cumulative_returns.min()) / cumulative_returns.max()
 
         # 计算 Calmar 比率
         calmar_ratio = ER / max_drawdown
-        print(calmar_ratio)
-        # profit_list = pd.Series(profit_list, index=self.spread.index)
-        # profit_list.to_csv('profit_list.csv')
-        self.profit = profit
+        print("Calmar Ratio:",calmar_ratio)
+        profit_list = pd.Series(profit_list, index=self.spread.index)
+        profit_list.to_csv('profit_list_delog.csv')
+        self.profit = profit/self.principle